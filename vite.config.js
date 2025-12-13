4fdda81abc0746c2ecc446860bf52e289ec96620
lineroolivaresorlandojose <lineroolivaresorlandojose@gmail.com>
2025-12-13 12:45:08 -0500

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/NASASHE-SAS/' : '/',
   plugins: [react()],
   // Permite que Vite resuelva los paquetes que exponen la condición "tauri",
   // necesaria para los plugins oficiales de Tauri cuando la app corre en el
